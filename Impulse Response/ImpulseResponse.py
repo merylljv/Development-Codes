41b82b27f42714661d0b34957ac9828dc4ce5587
Leodegario U. Lorenzo II <leolorenzoii@gmail.com>
2017-03-13 16:06:22 +0800

    LUL
    Impulse Response

--- Impulse Response/ImpulseResponse.py
+++ Impulse Response/ImpulseResponse.py
@@ -5,10 +5,10 @@ Created on Fri Feb 10 10:33:30 2017
 @author: Win8
 """
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import signal
 from scipy.optimize import minimize
+from scipy.ndimage import filters
 
 def step_fcn(t,A,d):
     return np.where(np.logical_and(t>=0,t<=d),1,0)*A
@@ -25,35 +25,51 @@ def exp1(t,T,D):
 def exp2(t,L):
     return np.exp(-t*np.log(2)/L)
 
-def transfer_function(t,T,D,L):
-    return np.convolve(exp1(t,T,D),exp2(t,L),'same')*(t[1]-t[0])
+def transfer_function(n,T,D,L):
+    t = np.arange(0,n)
+    return filters.convolve1d(exp1(t,T,D),exp2(t,L)/exp2(t,L))
 
 def model_output_signal(trans,input_signal,A):
-    return A*np.convolve(trans,input_signal,'same')
+    return A*filters.convolve1d(input_signal,trans)
 
-def optimize_fun(par,input_signal,output_signal,time_range):
-    trans = transfer_function(time_range,par[0],par[1],par[2])
+def optimize_fun(par,input_signal,output_signal,n):
+    trans = transfer_function(n,par[0],par[1],par[2])
     predicted = model_output_signal(trans,input_signal,par[3])
     return RMSE(predicted,output_signal)
-
-####MAIN
-####Use SLSQP to obtain transfer function
-
-##Input signals
-t = np.arange(0,150,1)
-test_input = 5.0*np.cos(t/7.14)+np.random.rand(len(t))
-test_output = 5.0*np.sin(t/7.14)+np.random.rand(len(t))
-bnds = ((0,None),(0,None),(0,None),(0,None))
-###Finding optimized parameters using SLSQP
-res = minimize(optimize_fun,(16,30.,1,5),args = (test_input,test_output,t),method = 'SLSQP',bounds = bnds)
-
-####Optimized transfer function
-print res
-
-optimized_trans = transfer_function(t,res.x[0],res.x[1],res.x[2])
-predicted_output = model_output_signal(optimized_trans,test_input,res.x[3])
-
-####Plot results
-plt.plot(t,test_input,label = 'Input')
-plt.plot(t,predicted_output,label = 'Predicted')
-plt.plot(t,test_output,label = 'Actual')
+def demo():
+    ###MAIN
+    ###Use SLSQP to obtain transfer function
+    
+    #Input signals
+    n = 50 ### length of IRF
+    
+    #### Generating test signals
+    t = np.arange(0,150,1)
+    test_input = 5.0*np.cos(t/7.14)+np.random.rand(len(t))
+    test_output = 2.0*np.sin(t/7.14)+np.random.rand(len(t))
+    
+    #### Parameter criteria
+    bnds = ((0,None),(0,None),(0,None),(0,None))
+    ###Finding optimized parameters using SLSQP
+    
+    ### Starting parameters
+    T = 13 #### Lag in acceleration phasse
+    D = 45 #### Duration of the acceleration phase
+    L = 1 #### Recession constant
+    A = 2/5. ### Normalization constant 
+    
+    
+    res = minimize(optimize_fun,(T,D,L,A),args = (test_input,test_output,n),method = 'SLSQP',bounds = bnds)
+    
+    ####Optimized transfer function
+    print res
+    
+    optimized_trans = transfer_function(n,res.x[0],res.x[1],res.x[2])
+    predicted_output = model_output_signal(optimized_trans,test_input,res.x[3])
+    
+    ####Plot results
+    plt.plot(t,test_input,label = 'Input')
+    plt.plot(t,predicted_output,label = 'Predicted')
+    plt.plot(t,test_output,label = 'Actual')
+    plt.legend(loc = 'upper left')
+    plt.show()
